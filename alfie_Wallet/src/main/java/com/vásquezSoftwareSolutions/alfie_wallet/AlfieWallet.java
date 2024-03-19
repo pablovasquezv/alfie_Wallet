d2f412a60ed708a910df684a06052a712c128114
Pablov <juan.vasquez@valposystem.com>
2024-03-18 23:26:29 -0300

    Creación de atributos para clase AlfieWallet.java

--- alfie_Wallet/src/main/java/com/vásquezSoftwareSolutions/alfie_wallet/AlfieWallet.java
+++ alfie_Wallet/src/main/java/com/vásquezSoftwareSolutions/alfie_wallet/AlfieWallet.java
@@ -4,9 +4,14 @@ package com.vásquezSoftwareSolutions.alfie_wallet;
  * @author Pablo
  */
 
+import java.util.List;
+
 /**
  * Clase: Usuario
  */
 public class AlfieWallet {
+    //Atributos
+    private double saldo;
 
+    private List<String> transacciones;
 }

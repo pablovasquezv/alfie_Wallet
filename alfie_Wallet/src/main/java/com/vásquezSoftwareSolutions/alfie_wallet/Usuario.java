0f2cec64125cbeec5852f9d3f43642755d71735e
Pablov <juan.vasquez@valposystem.com>
2024-03-18 23:14:13 -0300

    Creación de constructor vacío y con parámetros para clase Usuario.java

--- alfie_Wallet/src/main/java/com/vásquezSoftwareSolutions/alfie_wallet/Usuario.java
+++ alfie_Wallet/src/main/java/com/vásquezSoftwareSolutions/alfie_wallet/Usuario.java
@@ -13,4 +13,22 @@ public class Usuario {
     private String nombre;
     private AlfieWallet wallet;
 
+    /**
+     * Constructor Vacío
+     */
+    public Usuario() {
+    }
+
+    /**
+     * Constructor con parámetros
+     *
+     * @param id
+     * @param nombre
+     * @param wallet
+     */
+    public Usuario(int id, String nombre, AlfieWallet wallet) {
+        this.id = id;
+        this.nombre = nombre;
+        this.wallet = wallet;
+    }
 }

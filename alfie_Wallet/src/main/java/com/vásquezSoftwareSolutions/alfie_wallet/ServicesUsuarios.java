21f603589bf409e4458d2615a5cc5d677dc91d2e
Pablov <juan.vasquez@valposystem.com>
2024-03-19 16:48:23 -0300

    Creación de método para crear un usuario por consola.

--- alfie_Wallet/src/main/java/com/vásquezSoftwareSolutions/alfie_wallet/ServicesUsuarios.java
+++ alfie_Wallet/src/main/java/com/vásquezSoftwareSolutions/alfie_wallet/ServicesUsuarios.java
@@ -5,9 +5,23 @@ package com.vásquezSoftwareSolutions.alfie_wallet;
  * @version 1.0
  */
 
+import java.util.Scanner;
+
 /**
  * Clase: ServicesUsuarios
  */
 public class ServicesUsuarios {
+    Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Método que permite la creación de un usuario por consola.
+     * @return usuario
+     */
+    public Usuario crearUsuario() {
+
+        return usuario;
+    }
+
+
 
 }

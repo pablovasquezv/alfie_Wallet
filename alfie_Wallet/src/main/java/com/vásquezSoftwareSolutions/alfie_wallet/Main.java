44ec4407522702f9ea4fae45d26f0ece0203ae74
Pablov <juan.vasquez@valposystem.com>
2024-03-18 23:10:18 -0300

    Solución de importación tras cambio de nombre de paquete

--- alfie_Wallet/src/main/java/com/vásquezSoftwareSolutions/alfie_wallet/Main.java
+++ alfie_Wallet/src/main/java/com/vásquezSoftwareSolutions/alfie_wallet/Main.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.mycompany.alfie_wallet;
+package com.vásquezSoftwareSolutions.alfie_wallet;
 
 /**
  *

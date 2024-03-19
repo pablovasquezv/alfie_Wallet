0a4fc635e11cdac4311d8309c55fc60dd43522e0
Pablov <juan.vasquez@valposystem.com>
2024-03-18 23:16:53 -0300

    Creación de métods gett sett y toString para clase Usuario.java

--- alfie_Wallet/src/main/java/com/vásquezSoftwareSolutions/alfie_wallet/Usuario.java
+++ alfie_Wallet/src/main/java/com/vásquezSoftwareSolutions/alfie_wallet/Usuario.java
@@ -31,4 +31,58 @@ public class Usuario {
         this.nombre = nombre;
         this.wallet = wallet;
     }
+
+    /**
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return wallet
+     */
+    public AlfieWallet getWallet() {
+        return wallet;
+    }
+
+    /**
+     * @param wallet
+     */
+    public void setWallet(AlfieWallet wallet) {
+        this.wallet = wallet;
+    }
+
+    /**
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", wallet=" + wallet +
+                '}';
+    }
 }
